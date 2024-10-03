a3e62ca1835b963d5e1677e53723fcf3f370f4d0
ubuntu <joaoantoniosittamartins@gmail.com>
2024-10-03 14:56:34 -0300

    Aula1

---
+++ Aula1.py
@@ -0,0 +1,11 @@
+#Como realizar pergunta simples
+
+#nome = input('Digite seu nome: ')
+#print("{}, É um prazer te conhecer você".format(nome))
+
+#Como realizar uma soma simples
+valor1 = int(input('Digite um valor:'))
+valor2 = int(input('Digite um  outro valor:'))
+resultado = valor1 + valor2
+
+print("A soma é {}".format(resultado))
